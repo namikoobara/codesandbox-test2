6ec3c89e83fd9eee0e8c31428116ef69d8de366a
namikoobara <91645218+namikoobara@users.noreply.github.com>
2021-10-05 14:39:40 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * const let等の変数宣言
+ */
+
+// var val1="var変数";
+// console.log(val1);
+// //var変数は上書き可能
+// val1="var変数は上書き";
+// console.log(val1);
+// //var変数は再宣言可能
+// var val1="var変数を再宣言";
+// console.log(val1);
+
+//
+
+// const val3="const変数";
+// console.log(val3);
+
+// // val3="const overwritten";
+
+// const val3="const redeclared";
+
+// const val4={
+//   name:"じゃけえ",
+//   age:26,
+// };
+// val4.name ="jak";
+// console.log(val4);
+
+// const val5=['dog','cat'];
+// val5[0]='bird';
+// val5.push('monkey');
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+//  const name = "じゃけえ";
+//  const age = 28;
+//  const message1="私の名前は"+ name+ "です。年齢は"+age+ "です。";
+//  console.log(message1);
